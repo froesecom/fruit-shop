085d3dfcc34ffbe2ad379a037dde67c57fd4c47c
Erik Froese <froesecom@gmail.com>
2017-04-07 20:51:17 +1000

    successfully saving product attributes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,12 @@
 class OrdersController < ApplicationController
+  
   def create
+    @order = Order.new(order_params)
+  end
+
+  private
+  
+  def order_params
+    params.require(:order).permit(product_requests_attributes: [:product_id, :quantity])
   end
 end
