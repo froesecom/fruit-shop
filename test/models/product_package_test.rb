b8423e156623350fb579102dd547092edc87d4e9
Erik Froese <froesecom@gmail.com>
2017-04-09 17:42:01 +1000

    add productpackage tests and fixture data

--- test/models/product_package_test.rb
+++ test/models/product_package_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ProductPackageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should have a product" do
+    assert !ProductPackage.new(product_quantity: 1, value: 100).save   
+  end
+
+  test "should have a value" do
+    assert !ProductPackage.new(product_quantity: 1, product: products(:watermelon)).save
+  end
+
+  test "should have a product_quantity" do
+    assert !ProductPackage.new(value: 200, product: products(:rockmelon)).save
+  end
+
+
 end
