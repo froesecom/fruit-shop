cc59dc60350481ec5f048ad6745f02f2a699fc8a
Erik Froese <froesecom@gmail.com>
2017-04-11 07:25:34 +1000

    add order test for total

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,5 +1,13 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  
+ 
+  def setup
+    @empty_order = Order.new
+    @empty_order.save
+  end  
+
+  test "should have a total" do
+    assert !@empty_order.errors[:total].empty?
+  end
 end
