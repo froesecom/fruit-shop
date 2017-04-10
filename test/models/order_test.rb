6605ad6e174b197114bcad60e2126e2ccd85614d
Erik Froese <froesecom@gmail.com>
2017-04-11 08:13:14 +1000

    add order association tests

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -7,6 +7,10 @@ class OrderTest < ActiveSupport::TestCase
     @empty_order.save
   end  
 
+  #instance methods
+  
+  
+  #validations
   test "should have a total" do
     assert !@empty_order.errors[:total].empty?
   end
@@ -17,5 +21,26 @@ class OrderTest < ActiveSupport::TestCase
   
   test "should have order items" do
     assert !@empty_order.errors[:order_items].empty?
+  end
+  
+  #associations
+  test "should have have_many order items" do
+    o = Order.reflect_on_association(:order_items)
+    assert o.macro == :has_many
+  end
+
+  test "should have have_many product requests" do
+    o = Order.reflect_on_association(:product_requests)
+    assert o.macro == :has_many
+  end
+
+  test "should have have_many products" do
+    o = Order.reflect_on_association(:products)
+    assert o.macro == :has_many
+  end
+
+  test "should have have_many product packages" do
+    o = Order.reflect_on_association(:product_packages)
+    assert o.macro == :has_many
   end
 end
