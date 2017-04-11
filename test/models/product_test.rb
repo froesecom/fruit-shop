e7de4193bf6eb001f4dd6d28023d98f71aef402d
Erik Froese <froesecom@gmail.com>
2017-04-11 19:57:43 +1000

    add association tests for product

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,9 +1,26 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-
+  
+  #validations
   test "should have a name" do
     assert !Product.new.save
+  end
+
+  #associations
+  test "should have many orders" do
+    p = Product.reflect_on_association(:orders)
+    assert p.macro == :has_many
+  end
+
+  test "should have many product_requests" do
+    p = Product.reflect_on_association(:product_requests)
+    assert p.macro == :has_many
+  end
+
+  test "should have many product_packages" do
+    p = Product.reflect_on_association(:product_packages)
+    assert p.macro == :has_many
   end
 
 end
