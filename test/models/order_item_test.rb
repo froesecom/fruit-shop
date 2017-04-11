352955eea52ca49e87058906617f00d9d6301900
Erik Froese <froesecom@gmail.com>
2017-04-11 20:08:23 +1000

    add order items tests

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class OrderItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    
+  def setup
+    @empty_oi = OrderItem.new
+    @empty_oi.save
+  end
+  
+  #validations
+  test "should have order" do
+    assert !@empty_oi.errors[:order].empty?
+  end
+
+  test "should have product package" do
+    assert !@empty_oi.errors[:product_package].empty?
+  end
+
+  test "should have total" do
+    assert !@empty_oi.errors[:total].empty?
+  end
+
+  test "should have quantity" do
+    assert !@empty_oi.errors[:quantity].empty?
+  end
+
+  #associations 
+  test "should belong to order" do
+    oi = OrderItem.reflect_on_association(:order)
+    assert oi.macro == :belongs_to
+  end
+
+  test "should belong to product package" do
+    oi = OrderItem.reflect_on_association(:product_package)
+    assert oi.macro == :belongs_to
+  end
+
 end
