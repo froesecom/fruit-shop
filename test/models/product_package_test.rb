3343f80e9b4a47b5040db417ab3b4608b917f046
Erik Froese <froesecom@gmail.com>
2017-04-11 19:43:50 +1000

    write product package and product request tests

--- test/models/product_package_test.rb
+++ test/models/product_package_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ProductPackageTest < ActiveSupport::TestCase
-
+  # validations
   test "should have a product" do
     assert !ProductPackage.new(product_quantity: 1, value: 100).save   
   end
@@ -13,10 +13,27 @@ class ProductPackageTest < ActiveSupport::TestCase
   test "should have a product_quantity" do
     assert !ProductPackage.new(value: 200, product: products(:rockmelon)).save
   end
-
+  
+  #class methods
   test "for_target should not include product_packages larger than target " do
     packages = ProductPackage.for_target(5)
     assert packages.select {|p| p.product_quantity > 5}.empty?
+  end
+
+  #associations
+  test "should have belong_to product" do
+    pp = ProductPackage.reflect_on_association(:product)
+    assert pp.macro == :belongs_to
+  end
+
+  test "should have_many orders" do
+    pp = ProductPackage.reflect_on_association(:orders)
+    assert pp.macro == :has_many
+  end
+
+  test "should have_many order items" do
+    pp = ProductPackage.reflect_on_association(:order_items)
+    assert pp.macro == :has_many
   end
 
 end
--- test/models/product_request_test.rb
+++ test/models/product_request_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class ProductRequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ 
+  def setup
+    @empty_pr = ProductRequest.new
+    @empty_pr.save
+  end
+
+  #validations
+  test "should have order" do
+    assert !@empty_pr.errors[:order].empty?
+  end
+ 
+  test "should have product" do
+    assert !@empty_pr.errors[:product].empty?
+  end
+
+  test "should have can_fulfill true" do
+    assert !@empty_pr.errors[:can_fulfill].empty?
+  end
+
+  #associations
+  test "should belong to order" do
+    pp = ProductRequest.reflect_on_association(:order)
+    assert pp.macro == :belongs_to
+  end
+
+  test "should belong to product" do
+    pp = ProductRequest.reflect_on_association(:product)
+    assert pp.macro == :belongs_to
+  end
+
 end
